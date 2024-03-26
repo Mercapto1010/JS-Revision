da14461e228c91e2c980cb486d3c7b44953f8be6
Mercapto1010 <151073035+Mercapto1010@users.noreply.github.com>
2024-03-26 09:49:55 +0000

    Conversion of Datatypes

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,21 @@
+// let score = false
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); //NaN Not a Number
+
+// "69"=> 69
+//"69poi"=>NaN
+//true => 1; false => 0
+
+// let isloggedIn = "Yash"
+// let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(booleanIsloggedIn);
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
--- Basics/variables.js
+++ Basics/variables.js
@@ -2,10 +2,15 @@ const accountId = 12456
 let accountEmail = "yash@google.com"
 var accoountPassword = "12345"
 accountCity = "Jaunpur"
+let accountState;
 
 //accountId = 2 not allowed to make changes in constant
 accountEmail = "yoyo@gmail.com"
 accoountPassword = "2324"
 accountCity = "Varanasi"
 console.log(accountId);
-console.table([accountId,accountEmail,accoountPassword,accountCity]);
+/*
+prefer not to use var 
+because of issue in block scope and fucntional scope
+*/ 
+console.table([accountId,accountEmail,accoountPassword,accountCity,accountState]);
