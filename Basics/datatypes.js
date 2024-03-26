8ac54b3e53ffe9c76b40e678ebb5edd24dfd85ce
Mercapto1010 <151073035+Mercapto1010@users.noreply.github.com>
2024-03-26 09:06:44 +0000

    Datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,25 @@
+"Use strict"; //treat all JS code as newer version
+//alert(3+3) gives error because we are using nodejs not browser
+
+console.log(3+
+    3);
+//code readability should be high
+
+//DataTypes
+let name = "Yash"
+let age = 3
+let accountLoggedin = true
+let state = null
+
+//number => 2 to power 53
+//bigint
+// string => " "
+// boolean => true/false
+// null= standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof undefined); // output is undefined 
+console.log(typeof null); // output is object
---
+++ Basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 12456
+let accountEmail = "yash@google.com"
+var accoountPassword = "12345"
+accountCity = "Jaunpur"
+
+//accountId = 2 not allowed to make changes in constant
+accountEmail = "yoyo@gmail.com"
+accoountPassword = "2324"
+accountCity = "Varanasi"
+console.log(accountId);
+console.table([accountId,accountEmail,accoountPassword,accountCity]);
