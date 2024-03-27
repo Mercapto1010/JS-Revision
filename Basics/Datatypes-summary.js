fe9f2e383619e63ed2ef536b26431b15c44a203d
Mercapto1010 <151073035+Mercapto1010@users.noreply.github.com>
2024-03-27 17:28:10 +0000

    Datatypes summary

---
+++ Basics/Datatypes-summary.js
@@ -0,0 +1,31 @@
+// Primitive
+// 7 Types:- String Number Boolean Symbol Null Undefined BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('234')
+const anotherId = Symbol('234')
+console.log(id === anotherId);
+
+const bigNumber = 345234354654n
+
+// Reference(Non-Primitive)
+// Array Objects Functions
+
+const heroes = ["Shaktiman", "Sabu" ,"nagraj"]
+ 
+const myObj= {
+    name: "Yash",
+    age: 20,
+}
+
+const myFunction = function (){
+    console.log("YoYo Bantai");
+}
+
+console.log(typeof bigNumber);
