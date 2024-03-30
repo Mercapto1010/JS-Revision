8ad642395e49697c555eab4721104c30c409d58a
Mercapto1010 <151073035+Mercapto1010@users.noreply.github.com>
2024-03-30 17:26:50 +0000

    conversion done

---
+++ Basics/comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>4);
+// console.log(2<4);
+// console.log(2==4);
+// console.log(2>=4);
+// console.log(2<=4);
+
+//console.log("2">3);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+//=== this triple equal sign compares the datatypes
+
+console.log("2"===2);
