61b83ae65d31fa47117a5eb1973b77786a5eeee0
Mercapto1010 <151073035+Mercapto1010@users.noreply.github.com>
2024-04-07 07:20:46 +0000

    while and do-while loop

---
+++ iterations/two.js
@@ -0,0 +1,24 @@
+// while and do-while loop
+
+
+let i = 0
+// while (i<=10) {
+//     console.log(`Value of index is ${i}`);
+//     i = i+2
+// }
+
+let myArray = ["TradeX", "YourParking", "TradeX.ai"]
+
+let arr = 0
+
+while (arr< myArray.length) {
+    //console.log(`Ideas are ${myArray[arr]}`);
+    // arr = arr + 1
+    arr ++
+}
+
+let score = 100
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<= 10);
