019c3cc1bc55f250f0277200371ef5c162c9ab85
Mercapto1010 <151073035+Mercapto1010@users.noreply.github.com>
2024-04-07 08:26:15 +0000

    forof

---
+++ iterations/three.js
@@ -0,0 +1,50 @@
+//  for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const  arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+
+const greetings = "Hello World!"
+
+// for (const greet of greetings) {
+//     if(greet == " "){
+//         continue
+//     }
+//     console.log(`Each char is ${greet}`);
+    
+// }
+
+
+
+
+// Maps
+
+
+const map = new Map()
+
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('UK', "United Kingdom")
+map.set('IN', "India")
+
+// console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key, ':-', value);
+}
+
+
+const myObject = {
+    game1: 'GTA5',
+    game2: 'Aspalt'
+}
+
+// for (const [key,  value] of myObject) {
+//     console.log(key, ':-', value);
+// }
